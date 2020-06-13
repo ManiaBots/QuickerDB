a209857558ce6081c92113e84c914c5723c16b31
= <=>
2020-06-13 20:15:55 +0100

    Type's n stuff

--- src/Util/QuickerDB.ts
+++ src/Util/QuickerDB.ts
@@ -6,30 +6,30 @@ class QuickerDB {
     constructor(options:any={}) {
         this.debugMode = options.debug ? true : false;
     }
-    debugLog(a:any) {
+    debugLog(a:String):void {
         console.log(chalk.red.bold("[QuickerDB]"), chalk.yellow(a))
     }
 
-    add(a: String, b: Number): Number {
+    add(a:String, b:Number):Number {
         this.debugLog(`Adding ${chalk.blue.bold(b)} to ${chalk.blue.bold(a)}`);
         return db.add(a, b);
     }
 
-    remove(a: String): Boolean {
+    remove(a:String):Boolean {
         this.debugLog(`Deleting ${chalk.blue.bold(a)} from database.`);
         return db.delete(a);
     }
     
-    delete(a: String): Boolean {
+    delete(a:String):Boolean {
         return this.remove(a);
     }
 
-    fetch(a: String): any {
+    fetch(a:String):any {
         this.debugLog(`Fetching ${chalk.blue.bold(a)} from database.`);
         return db.fetch(a);
     }
 
-    get(a: String): any {
+    get(a:String):any {
         return this.fetch(a);
     }
 
