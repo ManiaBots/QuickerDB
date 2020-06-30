dd9372610165347ac363dcf9d78740eb0bf290ad
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2020-06-30 13:09:53 +0100

    Update QuickerDB.js

--- src/Util/QuickerDB.js
+++ src/Util/QuickerDB.js
@@ -1,44 +1,50 @@
-const db = require ("quick.db"), 
-    chalk = require("chalk");
+const db = require("quick.db"),
+  chalk = require("chalk");
 
 class QuickerDB {
-    constructor(options) {
-        this.debugMode = options.debug ? true : false;
-    };
+  constructor(options) {
+    this.debugMode = options.debug ? true : false;
+  }
 
-    debugLog(a) {
-        if(!this.debugMode) return;
-        else console.log(chalk.red.bold("[QuickerDB]"), chalk.yellow(a))
-    };
+  debugLog(a) {
+    if (!this.debugMode) return;
+    else console.log(chalk.red.bold("[QuickerDB]"), chalk.yellow(a));
+  }
 
-    add(a, b) {
-        this.debugLog(`Adding ${chalk.blue.bold(b)} to ${chalk.blue.bold(a)}`);
-        return db.add(a, b);
-    };
+  add(a, b) {
+    this.debugLog(`Adding ${chalk.blue.bold(b)} to ${chalk.blue.bold(a)}`);
+    return db.add(a, b);
+  }
 
-    mutliAdd(a) {
-        a.forEach(b => {
-            this.debugLog(`Adding ${chalk.blue.bold(b[1])} to ${chalk.blue.bold(b[0])}`);
-        });
-    };
+  multiAdd(a) {
+    a.forEach((b) => {
+      this.debugLog(
+        `Adding ${chalk.blue.bold(b[1])} to ${chalk.blue.bold(b[0])}`
+      );
+    });
+  }
 
-    remove(a) {
-        this.debugLog(`Deleting ${chalk.blue.bold(a)} from database.`);
-        return db.delete(a);
-    };
-    
-    delete(a) {
-        return this.remove(a);
-    };
+  remove(a) {
+    this.debugLog(`Deleting ${chalk.blue.bold(a)} from database.`);
+    return db.delete(a);
+  }
 
-    fetch(a, log) {
-        if(!log) this.debugLog(`Fetching ${chalk.blue.bold(a)} from database.`);
-        return db.fetch(a);
-    };
+  delete(a) {
+    return this.remove(a);
+  }
 
-    get(a) {
-        return this.fetch(a);
-    };
-};
+  fetch(a, log) {
+    if (!log) this.debugLog(`Fetching ${chalk.blue.bold(a)} from database.`);
+    return db.fetch(a);
+  }
+
+  get(a) {
+    return this.fetch(a);
+  }
+  push(a, b) {
+    return db.push(a, b);
+    this.debugLog(`Pushing ${chalk.blue.bold(b)} to ${chalk.blue.bold(a)}.`);
+  }
+}
 
 module.exports = QuickerDB;
